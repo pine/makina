817c7cf27f44009c31bcb2d921adedbb355c3bea
Pine Mizune <pinemz@gmail.com>
2015-05-11 07:51:35 +0900

    Update

--- lib/makina/sender.rb
+++ lib/makina/sender.rb
@@ -7,9 +7,11 @@ module Makina
   class Sender
     def initialize(log = nil)
       @log = log || Logger.new(STDOUT)
+      @skype = Makina::Skype::MainWindow.new
     end
     
     def dispose
+      @skype.dispose
     end
   end
 end
