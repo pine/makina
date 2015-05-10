119f1e09c9bdb274ef47b0422d57c8c5e2435ad7
Pine Mizune <pinemz@gmail.com>
2015-05-11 07:31:47 +0900

    Update

--- spec/sender_spec.rb
+++ spec/sender_spec.rb
@@ -6,9 +6,13 @@ require_relative './spec_helper'
 describe Makina::Sender do
   describe '#initialize' do
     it 'create instance' do
+      sender = nil
+      
       expect {
-        Makina::Sender.new
+        sender = Makina::Sender.new
       }.not_to raise_error
+      
+      sender.dispose rescue nil
     end
   end
 end
